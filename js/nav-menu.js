53cadc1641d580a6759098606b4e7d73ab9a3d5a
mrtsv <a.martsev85@gmail.com>
2021-10-24 23:33:50 +0300

    adapt menu

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,27 +1,12 @@
-var adaptNavPopup = document.querySelector(".adapt-nav-popup");
-var adaptPopupOpen = document.getElementById("adapt-popup-open");
-var adaptPopupClose = document.getElementById("adapt-popup-close");
+var adaptNavPopup = document.querySelector(".adapt_nav_popup");
+var adaptPopupOpen = document.getElementById("adapt_popup_open");
+var adaptPopupClose = document.getElementById("adapt_popup_close");
     
-adaptPopupOpen.onClick = function ()
+adaptPopupOpen.onclick = function ()
 {  
     adaptNavPopup.classList.add('active');
 }
-adaptPopupClose.onClick = function ()
+adaptPopupClose.onclick = function ()
 {  
     adaptNavPopup.classList.remove('active');
 }
-
-
-
-// var navHeaderAdapt = document.querySelector(".navigation__adapt__popup");
-// var openNavHeaderBtn = document.getElementById("navigationNavOpen");
-// var closeNavHeaderBtn = document.getElementById("navigationNavClose");
-    
-// openNavHeaderBtn.onclick = function ()
-// {  
-//     navHeaderAdapt.classList.add('active');
-// }
-// closeNavHeaderBtn.onclick = function ()
-// {  
-//     navHeaderAdapt.classList.remove('active');
-// }
